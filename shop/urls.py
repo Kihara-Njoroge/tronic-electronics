32dc97c151541fd441dd2a83365609adad9a4dd4
Babuuh <46190291+Babuuh@users.noreply.github.com>
2021-04-27 08:38:26 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from shop.views import(
     CheckoutPage, home,
-    cartPage, vendors, loginPage,
-    contactPage, registerPage, VendorRegisterPage,
+    cartPage, loginPage,
+    contactPage, registerPage,
     terms, privacy, faq, updateItem, processOrder,
-    productDetail, logoutUser, accountSettings,
+    productDetail, logoutUser,
 )
 
 urlpatterns = [
     path('', home, name='home'),
-    path('tronic-vendors/', vendors, name='vendors'),
     path('contact-us/', contactPage, name='contact'),
     path('cart/', cartPage, name='cart'),
     path('checkout/', CheckoutPage, name='checkout'),
@@ -18,10 +17,7 @@ urlpatterns = [
     path('login/', loginPage, name='login'),
     path('logout/', logoutUser, name='logout'),
     path('register/', registerPage, name='register'),
-    path('vendor_registration/', VendorRegisterPage, name='vendor_registration'),
     path('product_detail/<str:pk>/', productDetail, name='product_detail'),
-    path('settings/', accountSettings, name='settings'),
-
     path('terms&conditions/', terms, name='termsandconditions'),
     path('privacy-policy/', privacy, name='privacy-policy'),
     path('tronic-faq/',  faq, name='faq'),
